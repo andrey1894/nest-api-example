168625067fd7b6c9a196f1638973abc557739968
Andrey Lebedev <lebedev.andrey1894@gmail.com>
2022-07-06 17:13:31 +0300

    Feat: add jwt auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/users.model';
+import { AuthService } from './auth.service';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({ status: 200, type: User })
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authService.login(userDto);
+  }
+
+  @ApiOperation({ summary: 'Registration' })
+  @ApiResponse({ status: 200, type: User })
+  @Post('/registration')
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authService.registration(userDto);
+  }
+}
